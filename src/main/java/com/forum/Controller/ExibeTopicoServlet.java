8d3337095b4500640984d6fea984927ca02548c2
Igorpereirag <igorpereira1501@gmail.com>
2024-04-01 13:13:13 -0300

    feat:  servelet exibir topico

--- src/main/java/com/forum/Controller/ExibeTopicoServlet.java
+++ src/main/java/com/forum/Controller/ExibeTopicoServlet.java
@@ -7,10 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.forum.DAO.TopicoDAO;
+import com.forum.Model.Topico;
+
 @WebServlet("/exibeTopico")
 public class ExibeTopicoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      
-       
+        String idParam = request.getParameter("id");
+        if (idParam != null) {
+            int idTopico = Integer.parseInt(idParam);
+            TopicoDAO topicoDAO = new TopicoDAO();
+            Topico topico = topicoDAO.getTopicoById(idTopico);
+            if (topico != null) {
+                request.setAttribute("topico", topico);
+                request.getRequestDispatcher("/view/ExibeTopico.jsp").forward(request, response);
+            } else {
+              
+                response.sendRedirect(request.getContextPath() + "/erro.jsp");
+            }
+        } 
     }
 }
