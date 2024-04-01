a9114be22a784f30b93659833471aafff8e4a1f4
Igorpereirag <igorpereira1501@gmail.com>
2024-04-01 12:59:48 -0300

    feat: servelt para adicionar comentarios

---
+++ src/main/java/com/forum/Controller/AdicionarComentarioServlet.java
@@ -0,0 +1,37 @@
+package com.forum.Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.forum.DAO.ComentarioDAO;
+import com.forum.Model.Comentario;
+
+@WebServlet("/adicionar-comentario")
+public class AdicionarComentarioServlet extends HttpServlet {
+
+    private ComentarioDAO comentarioDAO;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        comentarioDAO = new ComentarioDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String texto = request.getParameter("texto");
+        String userId = request.getParameter("user_id");
+        int idTopico = Integer.parseInt(request.getParameter("id_topico"));
+
+        Comentario comentario = new Comentario(texto, userId, idTopico);
+
+        comentarioDAO.adicionarComentario(comentario);
+
+        
+        response.sendRedirect(request.getContextPath() + "/exibe-topico?id=" + idTopico);
+    }
+}
