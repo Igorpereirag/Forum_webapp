73b25dca6ce3ce610b1398d9036e90563aeab196
Igorpereirag <igorpereira1501@gmail.com>
2024-04-01 13:03:20 -0300

    feat: metodo para adicionar pontos ao inserir comentarios

--- src/main/java/com/forum/DAO/ComentarioDAO.java
+++ src/main/java/com/forum/DAO/ComentarioDAO.java
@@ -2,42 +2,47 @@ package com.forum.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import com.forum.Model.Comentario;
-import com.forum.Util.DatabaseUtil;
 import java.util.ArrayList;
 import java.util.List;
-import java.sql.ResultSet;
+
+import com.forum.Model.Comentario;
+import com.forum.Model.Usuario;
+import com.forum.Util.DatabaseUtil;
 
 public class ComentarioDAO {
     private final Connection connection;
+    private final UsuarioDAO usuarioDAO;
 
     public ComentarioDAO() throws SQLException {
         connection = DatabaseUtil.getConnection();
+        usuarioDAO = new UsuarioDAO();
     }
 
-   
     public void adicionarComentario(Comentario comentario) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO comentarios(texto, id_usuario_criador, id_topico) VALUES (?, ?, ?)");
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO comentarios(texto, id_usuario_criador, id_topico) VALUES (?, ?, ?)")) {
             preparedStatement.setString(1, comentario.getContent());
             preparedStatement.setString(2, comentario.getUser_id());
             preparedStatement.setInt(3, comentario.getIdTopico());
             preparedStatement.executeUpdate();
+
+           
+            Usuario usuario = usuarioDAO.getUsuarioById(comentario.getUser_id());
+            usuario.setPontos(usuario.getPontos() + 3);
+            usuarioDAO.atualizarPontosUsuario(usuario);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-   public List<Comentario> getComentarioByTopicId(int idTopico) {
-      List<Comentario> comentarios = new ArrayList<>();
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM comentarios WHERE topic_id = ?");
+
+    public List<Comentario> getComentarioByTopicId(int idTopico) {
+        List<Comentario> comentarios = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM comentarios WHERE topic_id = ?")) {
             preparedStatement.setInt(1, idTopico);
-            preparedStatement.executeQuery();
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                Comentario comentario = new Comentario(idTopico, resultSet.getString("content"), resultSet.getString("user_id"), resultSet.getInt("topic_id"));
+                Comentario comentario = new Comentario(idTopico, resultSet.getString("content"), resultSet.getString("user_id"));
                 comentarios.add(comentario);
             }
         } catch (SQLException e) {
@@ -45,6 +50,4 @@ public class ComentarioDAO {
         }
         return comentarios;
     }
-      
-    
 }
